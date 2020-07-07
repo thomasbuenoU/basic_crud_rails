0c3d5486cb5a5ad706c9808892958da76e6e6131
Thomas Bueno <thomasscolax@gmail.com>
2020-07-06 21:55:04 -0300

    Pegando todos os itens da lista
    A aplicação agora tem um DB integrado.
    
    criar uma nova tarefa pelo console:
    rails c
    Task.create(description: "Comprar cafe")
    
    O metodo all busca todas as entradas na tabela 'tasks' e para cada um, recria um objeto da class Task. Esses objetos são um grupo de array.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
   def welcome
-    @tasks = []
-    @tasks << Task.new(description: 'Comprar pão')
-    @tasks << Task.new(description: 'Comprar leite')
+    @tasks = Task.all
   end
 end
