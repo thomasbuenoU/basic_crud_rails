2483c7bf0e2f46b1f6d18605080946cd87eaa094
Thomas Bueno <thomasscolax@gmail.com>
2020-07-06 23:23:18 -0300

    Criando o metodo edit e update
    Agora temos que achar a tarefa que queremos editar e, através dos valores enviados
    pelo formulário, editar essa tarefa. Como já criamos uma tarefa, você já sabe receber
    parâmetros do formulário de maneira segura, só é necessário amarrar tudo.
    Pense que cada action trabalha de forma independente, ou seja, mesmo tendo
    procurado nossa tarefa em edit temos que buscá-la novamente no update, pois aquela
    busca anterior se perdeu ao sair da action.
    Editar é o mais simples, já que o Active Record nos dá um método chamado update
    para fazer a edição dos atributos. Depois podemos redirecionar para o root_path.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,4 +7,14 @@ class TasksController < ApplicationController
     @task.save
     redirect_to root_path
   end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params.require(:task).permit(:description, :status))
+    redirect_to root_path
+  end
 end
