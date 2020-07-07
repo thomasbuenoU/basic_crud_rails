e857de6daf0c561b41a138aa2034d12ebab38d25
Thomas Bueno <thomasscolax@gmail.com>
2020-07-06 21:50:32 -0300

    Adicionamos hashe como parametro

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def welcome
     @tasks = []
-    @tasks << Task.new('Comprar pão')
-    @tasks << Task.new('Comprar leite')
+    @tasks << Task.new(description: 'Comprar pão')
+    @tasks << Task.new(description: 'Comprar leite')
   end
 end
