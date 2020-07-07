081f42b74934ad76a15317a4a1e4702384f36b30
Thomas Bueno <thomasscolax@gmail.com>
2020-07-06 23:15:55 -0300

    Criando o metodo create no controller de tasks
    A action new só gera a página com o nosso formulário. Para enviar dados no HTTP nós
    usamos o verbo POST. Quando o usuário clica no botão ‘Criar’, nosso formulário já envia
    um POST com os dados da nossa tarefa para a action create. Agora temos que recolher
    esses dados e criar uma tarefa com eles.
    No controller, os dados vindos de qualquer formulário são recebidos por meio de uma
    variável chamada params. Essa variável é um hash e sua estrutura depende da requisição
    HTTP recebida. No exemplo da nossa tarefa, estamos recebendo o seguinte: { "task":
    { "description": "Comprar bolo", "status": true } }.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,10 @@
 class TasksController < ApplicationController
   def new
-    
+  end
+
+  def create
+    @task = Task.new(params.require(:task).permit(:description, :status))
+    @task.save
+    redirect_to root_path
   end
 end
